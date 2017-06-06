92b3ed5a9002b2fcd3f7f1c5cc96b6990fd6b70f
Sheshansh <sheshansh.agrawal@ist.ac.at>
2017-06-06 15:40:02 +0200

    changes

--- implementation/script.sh
+++ implementation/script.sh
@@ -1,6 +1,6 @@
 g++ Parser.cpp main.cpp -std=c++11 -g
 ./a.out < program
 /opt/ibm/ILOG/CPLEX_Studio_Community127/cplex/bin/x86-64_linux/cplex -c "read equations.lp" "optimize" "display solution variables *"
-rm a.out
-rm cplex.log
-rm equations.lp
+# rm a.out
+# rm cplex.log
+# rm equations.lp
