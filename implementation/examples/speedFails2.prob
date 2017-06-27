83271d94c348711ea99106f70c2d7dd5e3789c6e
Sheshansh <sheshansh.agrawal@ist.ac.at>
2017-06-27 14:12:47 +0200

    Examples with probability added into while loops

--- implementation/examples/speedFails2.prob
+++
@@ -1,4 +0,0 @@
-var i,x,n;
-[x>n]
-i:=x;
-while i>=n+1 or i+1<=n do i:=i+1 od
--- implementation/examples/terminate.c
+++
@@ -1,9 +0,0 @@
-int terminate(){
-  int i, j, k, ell;
-  while(i <= 100 && j <= k){
-    ell = i;
-    i = j;
-    j = ell+1;
-    k--;
-  }
-}
